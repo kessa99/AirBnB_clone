c2974667a101b7e75eeaa82b8fd7b228d034fd49
kessa99 <108158840+kessa99@users.noreply.github.com>
2023-05-14 10:09:11 +0000

    Create the first base_model

--- models/base_model.py
+++ models/base_model.py
@@ -9,22 +9,23 @@ this constructor defines the id attributea and some methods
 class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
-    def str(self):
+    def __str__(self):
         """
         We have to print the initialized information information
         for printing the name of the class, we have two choices:
-        1- class_name = self.__class__.name and call for format(class__name__)
-        2- format(type(self).name)
+        1- class_name = self.__class__.__name__ and call for format(class__name__)
+        2- format(type(self).__name__)
         dict: it automatically transform the initialized element into dictionary
         """
-        return "[{}] ({}) {}".format(type(self).name, self.id, self.dict)
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.dict)
 
     def save(self):
         """
         update the public instance attribute
         """
-        self.created_at = datatime.now()
         self.updated_at = datetime.now()
 
 
@@ -34,8 +35,8 @@ class BaseModel:
         The isoformat is use to print with precesion time.
         """
         
-        my_dict = self.dict.copy()
-        my_dict['class'] = self.__classname__
+        my_dict = self.__dict__.copy()
+        my_dict['class'] = type(self).__name__
         my_dict['created_at'] = self.created_at.isoformat()
         my_dict['update_at'] = self.updated_at.isoformat()
         return my_dict
