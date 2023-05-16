571b8210f55a9bee14365331572a98736bf9e407
Ojobumiche <bumiche@gmail.com>
2023-05-16 16:00:23 -0700

    place

--- models/place.py
+++ models/place.py
@@ -2,28 +2,30 @@
 from models.base_model import BaseModel
 
 class Place(BaseModel):
-    """Signify a place.
+    """Signify a place class.
 
     Attributes:
-        city_id: string-empty string
-        user_id : atring-empty string
-        name : name in string
-        description : The description of the place in string
-        number_rooms : The number of rooms type int
-        number_bathrooms : The number of bathrooms is an int
-        max_gest : number of guest int type
-        price_by_night : the prix attribuate
-        latitude :  coordonate of place
-        longitude : second coordonne of place
+        city_id (str): The ID of the associated City.
+        user_id (str): The ID of the associated User.
+        name (str): The name of the place.
+        description (str): The description of the place.
+        number_rooms (int): The number of rooms in the place.
+        number_bathrooms (int): The number of bathrooms in the place.
+        max_guest (int): The maximum number of guests allowed in the place.
+        price_by_night (int): The price per night for the place.
+        latitude (float): The latitude coordinate of the place.
+        longitude (float): The longitude coordinate of the place.
+        amenity_ids (list): A list of IDs of associated Amenities.
     """
-        city_id = ""
-        user_id = ""
-        name = ""
-        description = ""
-        number_rooms = 0
-        number_bathrooms = 0
-        max_guest = 0
-        price_by_night = 0
-        latitude = 0.0
-        longitude = 0.0
-        amenity_ids = []
+
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
