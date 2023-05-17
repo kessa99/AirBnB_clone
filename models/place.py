934ea0ca1d9d89db001a329a4c0900809709c239
Ojobumiche <bumiche@gmail.com>
2023-05-17 05:12:33 -0700

    place file

--- models/place.py
+++ models/place.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""Defines the Place class."""
 from models.base_model import BaseModel
 
+
 class Place(BaseModel):
-    """Signify a place class.
+    """Represents a place.
 
     Attributes:
         city_id (str): The ID of the associated City.
